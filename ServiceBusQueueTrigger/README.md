31d65c19030b4a104e52b4f8e09a800fca371ebb
tuyennguyen99 <nguyentuyen95.bt@gmail.com>
2023-04-17 00:46:48 +0700

    .

---
+++ ServiceBusQueueTrigger/README.md
@@ -0,0 +1,21 @@
+pg_restore -h prj3db.postgres.database.azure.com -p 5432 -U prj3admin@prj3db --dbname=techconfdb --verbose techconfdb_backup.tar
+    "DbConnection": "dbname='techconfdb' user='prj3admin@prj3db' host='prj3db.postgres.database.azure.com' password='@1995N89y12345' port='5432'"
+    "SENDGRID_API_KEY": "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",
+    "ADMIN_EMAIL_ADDRESS": "nguyentuyen.bt95@yahoo.com",
+"SENDGRID_API_KEY": "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",
+    "ADMIN_EMAIL_ADDRESS": "nguyentuyen.bt95@yahoo.com",
+    "DbConnection": "dbname='techconfdb' user='postgres' host='localhost' password='' port='5432'"
+{
+  "IsEncrypted": false,
+  "Values": {
+    "AzureWebJobsStorage": "DefaultEndpointsProtocol=https;AccountName=prj3rsgroup9c28;AccountKey=oB78pzWb2EAsYFJjMmjmvgJm1DBgolEGPs5z7NnYmCj0cLPKjUeIUAK1B5Q47zj68PPLGVOpfn9w+AStZMquXA==;EndpointSuffix=core.windows.net",
+    "FUNCTIONS_WORKER_RUNTIME": "python",
+    "prj3servicebus01_SERVICEBUS": "Endpoint=sb://prj3servicebus01.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=3tm8RiwTdNzMQt6n7jvpavtgyrmIaU4KQ+ASbBZuC+s=",
+    "APPINSIGHTS_INSTRUMENTATIONKEY": "2d01cb8f-e372-466a-88c9-b1eb074d0379",
+    "APPLICATIONINSIGHTS_CONNECTION_STRING": "InstrumentationKey=2d01cb8f-e372-466a-88c9-b1eb074d0379;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/",
+    "FUNCTIONS_EXTENSION_VERSION": "~4",
+    "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING": "DefaultEndpointsProtocol=https;AccountName=prj3rsgroup9c28;AccountKey=oB78pzWb2EAsYFJjMmjmvgJm1DBgolEGPs5z7NnYmCj0cLPKjUeIUAK1B5Q47zj68PPLGVOpfn9w+AStZMquXA==;EndpointSuffix=core.windows.net",
+    "WEBSITE_CONTENTSHARE": "prj3func01ab21",
+    "DbConnection": "dbname='techconfdb' user='postgres' host='localhost' password='' port='5432'"
+  }
+}
--- ServiceBusQueueTrigger/__init__.py
+++ ServiceBusQueueTrigger/__init__.py
@@ -1,14 +1,60 @@
 import logging
 import psycopg2
 import azure.functions as func
-
+import os
+import datetime
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 def main(msg: func.ServiceBusMessage):
-    notification_id = int(msg.get_body().decode('utf-8'))
+    notification_id = msg.get_body().decode('utf-8')
     logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
     # TODO: Get connection to database
     try:
+        url = os.environ['DbConnection']
+        strs = url.replace('\'', '').split(' ')
+        userTmpl = 'user='
+        dbNameTmpl = 'dbname='
+        hostTmpl = 'host='
+        portTmpl = 'port='
+        pwdTmpl = 'password='
+
+        for s in strs:
+            if userTmpl in s:
+                user = s.replace(userTmpl, '')
+            if dbNameTmpl in s:
+                dbname = s.replace(dbNameTmpl, '')
+            if hostTmpl in s:
+                host = s.replace(hostTmpl, '')
+            if portTmpl in s:
+                port = s.replace(portTmpl, '')
+            if pwdTmpl in s:
+                password = s.replace(pwdTmpl, '')
+        connection = psycopg2.connect(database = dbname, 
+                        user = user, 
+                        host= host,
+                        password = password,
+                        port = port)
         logging.info('try')
+        cursor = connection.cursor()
+        getNotificationsQuery = 'SELECT id, status, message, submitted_date, completed_date, subject FROM public.notification WHERE id = ' + notification_id
+        cursor.execute(getNotificationsQuery)
+        notification = cursor.fetchone()
+        notification_message = notification[2]
+        notification_subject = notification[3]
+
+        getAttendeesQuery = 'SELECT id, first_name, last_name, conference_id, job_position, email, company, city, state, interests, submitted_date, comments FROM public.attendee;'
+        cursor.execute(getAttendeesQuery)
+        attendees = cursor.fetchall()
+        count = 0
+        for attendee in attendees:
+            count += 1
+            first_name = attendee[1]
+            email = attendee[5]
+            subject = '{}: {}'.format(first_name, notification_subject)
+            send_email(email, subject, notification_message)
+            logging.info('Email has been sent to {} for attendee {}'.format(email, first_name))
+        
         # TODO: Get notification message and subject from database using the notification_id
 
         # TODO: Get attendees email and name
@@ -16,7 +62,26 @@ def main(msg: func.ServiceBusMessage):
         # TODO: Loop through each attendee and send an email with a personalized subject
 
         # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
+        status = 'Notified {attendeeCount} attendees'.format(attendeeCount = str(count))
+        cursor.execute('UPDATE public.notification SET status=%s, completed_date=%s WHERE id = %s', (status, datetime.datetime.now(), notification_id))
+
+        connection.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
     finally:
-        logging.info('finally')
+        logging.info('Closing connection')
+        connection.close()
+
+def send_email(email, subject, body):
+    send_grid_api_key = str(os.environ['SENDGRID_API_KEY'])
+    if send_grid_api_key is None or send_grid_api_key == '':
+        return
+    message = Mail(
+            from_email=os.environ['ADMIN_EMAIL_ADDRESS'],
+            to_emails=email,
+            subject=subject,
+            plain_text_content=body)
+
+    sg = SendGridAPIClient(send_grid_api_key)
+    sg.send(message)
+        
